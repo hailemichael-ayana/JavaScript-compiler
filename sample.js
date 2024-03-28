75924c0e7eb4aabc4ab1e6ceca038ed481c17ab5
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2024-03-29 00:16:21 +0300

    sample js

---
+++ sample.js
@@ -0,0 +1,17 @@
+const greeting = "Hello, World!";
+const greg = 'Hello, World!';
+let count = 0;
+
+function increment() {
+    count++;
+    console.log("Count:", count);
+}
+
+let fruits = ['Apple', 'Banana', 'Orange'];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log("Fruit:", fruits[i]);
+}
+
+increment();
+increment();
